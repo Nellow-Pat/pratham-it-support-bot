dd0f1da93c7d5a3f39d9978bf61c25f3e492fc5b
Nellow <prabeshpathak002@gmail.com>
2025-06-22 15:13:03 +0545

    Update chat.auth.endpoints.ts

--- src/api/constants/chat.auth.endpoints.ts
+++ src/api/constants/chat.auth.endpoints.ts
@@ -4,5 +4,5 @@ export const CHAT_PREFIX = `${API_V1_PREFIX}/chat`;
 
 export const LOGIN_USER_ENDPOINT = `${AUTH_PREFIX}/login/user`;
 
-export const CREATE_CHAT_ENDPOINT = `${CHAT_PREFIX}/create`;
+export const CREATE_CHAT_ENDPOINT = `${CHAT_PREFIX}/chats`;
 export const STREAM_CHAT_ENDPOINT = `${CHAT_PREFIX}/stream`;
